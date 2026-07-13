b251091c69b1b561005e17c8adc6e78228463ec5
Lucas Sung <lucas.sung09@hotmail.com>
2026-07-13 19:58:24 +0100

    Before mobile navigation update

---
+++ script.js
@@ -0,0 +1,10 @@
+const menuButton = document.querySelector(".menu-toggle");
+
+const navLinks = document.querySelector(".nav-links");
+
+
+menuButton.addEventListener("click", () => {
+
+    navLinks.classList.toggle("active");
+
+});
